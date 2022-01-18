401e50706cd2c09d498a1b989dbddb843f644549
bruceqiq <18228937997@163.com>
2022-01-18 23:20:25 +0800

    update

--- test.js
+++
@@ -1,9 +0,0 @@
-const qn = require('qiniu')
-var accessKey = 'tCtyvBZG6Z1BgchTf7cBb1PskRSrgiFTw7E6kbGq'
-var secretKey = 'vRe5BCgITnAUFfLnYw2OliPXXsB6qesOVrRVdGZI'
-var mac = new qn.auth.digest.Mac(accessKey, secretKey)
-var options = {
-  scope: 'emoji-img'
-}
-var putPolicy = new qn.rs.PutPolicy(options)
-console.log(putPolicy.uploadToken(mac))
