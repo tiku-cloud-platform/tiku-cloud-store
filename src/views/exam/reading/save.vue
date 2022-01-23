fa0281633864fa123cc2914fcf502c2112f0ce9a
bruceqiq <18228937997@163.com>
2022-01-23 23:56:49 +0800

    update

--- src/views/exam/reading/save.vue
+++ src/views/exam/reading/save.vue
@@ -79,13 +79,6 @@
           </el-col>
         </el-row>
         <div class="dividerTitle">
-          <span class="title mr10">试题内容</span>
-          <el-divider />
-        </div>
-        <el-form-item label="试题描述：" prop="content">
-          <ueditor-from v-model="formValidate.content" :content="formValidate.content" />
-        </el-form-item>
-        <div class="dividerTitle">
           <span class="title">其他设置</span>
           <el-divider />
         </div>
@@ -113,6 +106,13 @@
             </el-form-item>
           </el-col>
         </el-row>
+        <div class="dividerTitle">
+          <span class="title mr10">试题内容</span>
+          <el-divider />
+        </div>
+        <el-form-item label="试题描述：" prop="content">
+          <ueditor-from v-model="formValidate.content" :content="formValidate.content" />
+        </el-form-item>
         <el-button type="primary" class="submission" @click="onsubmit('formValidate')">提交</el-button>
       </el-form>
     </el-card>
